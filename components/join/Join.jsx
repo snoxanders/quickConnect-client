b5753c201e8db19c494f3e864df238318144fd43
unknown <alexandre.gcms@gmail.com>
2024-10-31 00:15:30 +0000

    add frontend connec

--- components/join/Join.jsx
+++ components/join/Join.jsx
@@ -6,14 +6,13 @@ export default function Join({ setChatVisibility, setSocket }) {
   const usernameRef = useRef();
 
   const handleSubmit = async () => {
-    const username = usernameRef.current.value;
-    if (!username.trim()) return;
-    const socket = await io.connect('https://quickconnect-server-production.up.railway.app');
-
-    socket.emit('set_username', username);
-    setSocket(socket);
+    const username = usernameRef.current.value
+    if(!username.trim()) return 
+    const socket = await io.connect('http://localhost:3001')
+    socket.emit('set_username', username)
+    setSocket(socket)
     setChatVisibility(true);
-  };
+  }
 
   return (
     <div className="join-container">
