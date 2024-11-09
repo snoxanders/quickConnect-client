22e4c90bb7de8f3730020d0f015dd8d2891027a8
unknown <alexandre.gcms@gmail.com>
2024-11-09 21:32:07 +0000

    eae

--- components/join/Join.jsx
+++ components/join/Join.jsx
@@ -8,7 +8,7 @@ export default function Join({ setChatVisibility, setSocket }) {
   const handleSubmit = async () => {
     const username = usernameRef.current.value
     if(!username.trim()) return 
-    const socket = await io.connect('http://localhost:3001')
+    const socket = await io.connect('quickconnect-server-production.up.railway.app')
     socket.emit('set_username', username)
     setSocket(socket)
     setChatVisibility(true);
